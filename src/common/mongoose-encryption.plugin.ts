c15603f30c20ae240078cfb3e36e2b5fce297b55
paulocayres <paulo.cayres@gmail.com>
2025-08-20 20:26:37 -0300

    nova correção para manter somente o owwnerid sem criptografia

--- src/common/mongoose-encryption.plugin.ts
+++ src/common/mongoose-encryption.plugin.ts
@@ -8,7 +8,7 @@ function isObject(v: any) {
 function traverseAndEncrypt(obj: any, encSvc: EncryptionService) {
   if (!obj || typeof obj !== 'object') return;
   for (const k of Object.keys(obj)) {
-    if (k === '_id' || (k !== 'valor' && k !== 'descricao')) continue; // Ignore _id and encrypt only 'valor' and 'descricao'
+    if (k === '_id' || k === 'ownerId' || (k !== 'valor' && k !== 'descricao')) continue; // Ignore _id, ownerId, and encrypt only 'valor' and 'descricao'
     const v = obj[k];
     if (v === null || v === undefined) continue;
     if (typeof v === 'string' || typeof v === 'number' || typeof v === 'boolean') {
@@ -20,7 +20,7 @@ function traverseAndEncrypt(obj: any, encSvc: EncryptionService) {
 function traverseAndDecrypt(obj: any, encSvc: EncryptionService) {
   if (!obj || typeof obj !== 'object') return;
   for (const k of Object.keys(obj)) {
-    if (k === '_id' || (k !== 'valor' && k !== 'descricao')) continue; // Ignore _id and decrypt only 'valor' and 'descricao'
+    if (k === '_id' || k === 'ownerId' || (k !== 'valor' && k !== 'descricao')) continue; // Ignore _id, ownerId, and decrypt only 'valor' and 'descricao'
     const v = obj[k];
     if (v === null || v === undefined) continue;
     if (typeof v === 'string' && encSvc.looksEncrypted(v)) {
@@ -63,7 +63,7 @@ export function createEncryptionPlugin(encSvc: EncryptionService) {
         function encryptInPlace(obj: any) {
           if (!obj || typeof obj !== 'object') return;
           for (const k of Object.keys(obj)) {
-            if (k === '_id' || k === 'uid') continue; // Ignore _id and uid
+            if (k === '_id' || k === 'uid' || k === 'ownerId') continue; // Ignore _id, uid, and ownerId
             const v = obj[k];
             if (v === null || v === undefined) continue;
             if (typeof v === 'string' || typeof v === 'number' || typeof v === 'boolean') {
